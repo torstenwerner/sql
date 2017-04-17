b5037ebb50812c8280cde69116fd3accb0d41cef
Torsten Werner <torsten.werner@westernacher.com>
2017-04-17 11:08:19 +0200

    more tasks

--- tutorial.sql
+++ tutorial.sql
@@ -1,28 +1,108 @@
-select * from city;
+-- check if language percentages add up to 100%
+SELECT
+  countrycode,
+  sum(percentage)
+FROM countrylanguage
+GROUP BY countrycode;
 
-select countrycode, sum(percentage) from countrylanguage GROUP BY countrycode;
-
-SELECT countrycode, language, percentage, rank() OVER (PARTITION BY countrycode ORDER BY percentage DESC)
-  FROM countrylanguage ORDER BY countrycode, percentage DESC;
-
-select c.name, to_char((c.population * cl.percentage) / 100, '9999999999') as speakers, cl.language
-  from country c JOIN countrylanguage cl on c.code = cl.countrycode;
-
-select to_char(sum(c.population * cl.percentage) / 100, '9999999999') as speakers, cl.language
-  from country c JOIN countrylanguage cl on c.code = cl.countrycode
-  GROUP BY cl.language ORDER BY speakers DESC;
-
-select sum(population) from country;
-
-select
-  to_char(sum(c.population * cl.percentage) / 100 / (select sum(population) from country), '0.999999') as percentage,
+-- calculate rounded number of speakers of each language in each country
+SELECT
+  c.name,
+  round((c.population * cl.percentage) / 100) AS speakers,
   cl.language
-  from country c JOIN countrylanguage cl on c.code = cl.countrycode
-  GROUP BY cl.language ORDER BY percentage DESC;
+FROM country c
+  JOIN countrylanguage cl ON c.code = cl.countrycode;
 
-WITH pl AS (SELECT
-  to_char(sum(c.population * cl.percentage) / 100 / (select sum(population) from country), '0.999999') as percentage,
+-- calculate rounded number of speakers of each language in the world; sort by speakers descending
+SELECT
+  round(sum(c.population * cl.percentage) / 100) AS speakers,
   cl.language
-from country c JOIN countrylanguage cl on c.code = cl.countrycode
-GROUP BY cl.language ORDER BY percentage DESC)
-SELECT row_number() OVER () as rank, percentage, language FROM pl;
+FROM country c
+  JOIN countrylanguage cl ON c.code = cl.countrycode
+GROUP BY cl.language
+ORDER BY speakers DESC;
+
+-- calculate the world population
+SELECT sum(population)
+FROM country;
+
+-- calculate the percentage of each language in the world; format percentage like 0.123456; sort by percentage descending
+SELECT
+  to_char(sum(c.population * cl.percentage) / 100 / (SELECT sum(population)
+                                                     FROM country), '0.999999') AS percentage,
+  cl.language
+FROM country c
+  JOIN countrylanguage cl ON c.code = cl.countrycode
+GROUP BY cl.language
+ORDER BY percentage DESC;
+
+-- as above but add a rank column e.g. chinese 1, hindi 2, ...
+SELECT
+  row_number()
+  OVER (
+    ORDER BY percentage DESC )    AS rank,
+  language,
+  to_char(percentage, '0.999999') AS percentage
+FROM (
+       SELECT
+         sum(c.population * cl.percentage) / 100 / (SELECT sum(population)
+                                                    FROM country) AS percentage,
+         cl.language
+       FROM country c
+         JOIN countrylanguage cl ON c.code = cl.countrycode
+       GROUP BY cl.language) AS pl;
+
+-- sort countries by population descending and add a rank column e.g. china 1, india 2
+SELECT
+  code,
+  name,
+  row_number()
+  OVER (
+    ORDER BY population DESC ) AS rank,
+  population
+FROM country;
+
+-- calculated rank of language – sorted by percentage descending – in each country
+SELECT
+  c.code,
+  cl.language,
+  row_number()
+  OVER (
+    PARTITION BY countrycode
+    ORDER BY percentage DESC ) AS rank,
+  cl.percentage
+FROM country c
+  JOIN countrylanguage cl ON c.code = cl.countrycode;
+
+-- combine the tasks above – display 6 columns sorted by population rank and language rank:
+-- country name, rank of country by population, population, language, rank of language within the country, percentage
+SELECT
+  cr.name,
+  cr.rank AS poprank,
+  cr.population,
+  lr.language,
+  lr.rank AS langrank,
+  lr.percentage
+FROM (
+       SELECT
+         code,
+         name,
+         row_number()
+         OVER (
+           ORDER BY population DESC ) AS rank,
+         population
+       FROM country
+     ) AS cr
+  JOIN (
+         SELECT
+           c.code,
+           cl.language,
+           row_number()
+           OVER (
+             PARTITION BY countrycode
+             ORDER BY percentage DESC ) AS rank,
+           cl.percentage
+         FROM country c
+           JOIN countrylanguage cl ON c.code = cl.countrycode) AS lr
+    ON cr.code = lr.code
+ORDER BY cr.rank, lr.rank;

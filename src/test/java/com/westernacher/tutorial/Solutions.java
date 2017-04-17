2f6f2faff956f045d7cbd1724d0b48e5e40ca930
Torsten Werner <torsten.werner@westernacher.com>
2017-04-17 18:44:20 +0200

    implement 2nd java solution

--- src/test/java/com/westernacher/tutorial/Solutions.java
+++ src/test/java/com/westernacher/tutorial/Solutions.java
@@ -6,6 +6,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import java.util.Map;
+import java.util.function.Function;
+
 import static java.util.stream.Collectors.toMap;
 
 @RunWith(SpringRunner.class)
@@ -20,5 +23,32 @@ public class Solutions {
         worldRepository.findAllLanguages().stream()
                 .collect(toMap(Language::getCountrycode, Language::getPercentage, (sum, value) -> sum + value))
                 .forEach((countrycode, percentage) -> System.out.printf("%s %.1f\n", countrycode, percentage));
+    }
+
+    @Test
+    public void speakersPerCountry() throws Exception {
+        final Map<String, Country> countries = worldRepository.findAllCountries().stream()
+                .collect(toMap(Country::getCode, Function.identity()));
+
+        class ResultRow {
+            String name;
+            long speakers;
+            String language;
+
+            ResultRow(Language language) {
+                final Country country = countries.get(language.getCountrycode());
+                this.name = country.getName();
+                this.speakers = Math.round(country.getPopulation() * language.getPercentage());
+                this.language = language.getLanguage();
+            }
+
+            void print() {
+                System.out.printf("%40s %10d %30s\n", name, speakers, language);
+            }
+        }
+
+        worldRepository.findAllLanguages().stream()
+                .map(language -> new ResultRow(language))
+                .forEach(ResultRow::print);
     }
 }

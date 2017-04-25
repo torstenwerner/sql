2159d2e0db5dc1cd1454b767b3825d18195bda1e
Torsten Werner <torsten.werner@westernacher.com>
2017-04-25 21:19:23 +0200

    fix ordering

--- maybesql.sql
+++ maybesql.sql
@@ -38,17 +38,19 @@ WHERE a0.userid IN (
 
 SELECT a0.*
 FROM attribute a0
-  JOIN attribute s ON a0.userid = s.userid
-WHERE s.key = 'givenName' AND a0.userid IN (
+  JOIN attribute s0 ON a0.userid = s0.userid
+WHERE s0.key = 'givenName' AND a0.userid IN (
   SELECT a1.userid
   FROM attribute a1
-  WHERE a1.key = 'l' AND a1.value ILIKE '%Berlin%'
+    JOIN attribute s1 ON a1.userid = s1.userid
+  WHERE s1.key = 'givenName' AND a1.key = 'l' AND a1.value ILIKE '%Berlin%'
         AND exists(SELECT 1
                    FROM attribute a2
                    WHERE a2.userid = a1.userid AND a2.key = 'sn' AND a2.value ILIKE '%Muster%'
                          AND exists(SELECT 1
                                     FROM attribute a3
                                     WHERE a3.userid = a1.userid AND a3.key = 'givenName' AND a3.value ILIKE '%a%'))
+  ORDER BY s1.value DESC
   OFFSET 0
   LIMIT 2)
-ORDER BY s.value DESC;
+ORDER BY s0.value DESC;

8a4b1c7c24d0fc49f6c4d9b736aaf7a30493e332
Torsten Werner <torsten.werner@westernacher.com>
2017-04-25 20:51:14 +0200

    add MaybeSQL sample

---
+++ attribute_dump.sql
@@ -0,0 +1,12 @@
+INSERT INTO public.attribute (userid, key, value) VALUES ('hans', 'givenName', 'Hans');
+INSERT INTO public.attribute (userid, key, value) VALUES ('hans', 'sn', 'Muster');
+INSERT INTO public.attribute (userid, key, value) VALUES ('hans', 'l', 'Berlin');
+INSERT INTO public.attribute (userid, key, value) VALUES ('anna', 'givenName', 'Anna');
+INSERT INTO public.attribute (userid, key, value) VALUES ('anna', 'sn', 'Lehmann');
+INSERT INTO public.attribute (userid, key, value) VALUES ('anna', 'l', 'Berlin');
+INSERT INTO public.attribute (userid, key, value) VALUES ('max', 'givenName', 'Max');
+INSERT INTO public.attribute (userid, key, value) VALUES ('max', 'sn', 'Muster');
+INSERT INTO public.attribute (userid, key, value) VALUES ('max', 'l', 'Berlin');
+INSERT INTO public.attribute (userid, key, value) VALUES ('nina', 'givenName', 'Nina');
+INSERT INTO public.attribute (userid, key, value) VALUES ('nina', 'sn', 'Meyer');
+INSERT INTO public.attribute (userid, key, value) VALUES ('nina', 'l', 'Hamburg');
---
+++ maybesql.sql
@@ -0,0 +1,54 @@
+-- some kind of MaybeSQL database
+
+SELECT a1.*
+FROM attribute a1
+WHERE a1.key = 'l' AND a1.value ILIKE '%Berlin%';
+
+SELECT a0.*
+FROM attribute a0
+WHERE a0.userid IN (
+  SELECT a1.userid
+  FROM attribute a1
+  WHERE a1.key = 'l' AND a1.value ILIKE '%Berlin%');
+
+SELECT a0.*
+FROM attribute a0
+WHERE a0.userid IN (
+  SELECT a1.userid
+  FROM attribute a1
+  WHERE a1.key = 'l' AND a1.value ILIKE '%Berlin%'
+        AND exists(SELECT 1
+                   FROM attribute a2
+                   WHERE a2.userid = a1.userid AND a2.key = 'sn' AND a2.value ILIKE '%Muster%'));
+
+SELECT a0.*
+FROM attribute a0
+WHERE a0.userid IN (
+  SELECT a1.userid
+  FROM attribute a1
+  WHERE a1.key = 'l' AND a1.value ILIKE '%Berlin%'
+        AND exists(SELECT 1
+                   FROM attribute a2
+                   WHERE a2.userid = a1.userid AND a2.key = 'sn' AND a2.value ILIKE '%Muster%'
+                         AND exists(SELECT 1
+                                    FROM attribute a3
+                                    WHERE a3.userid = a1.userid AND a3.key = 'givenName' AND a3.value ILIKE '%a%'))
+  OFFSET 0
+  LIMIT 2);
+
+SELECT a0.*
+FROM attribute a0
+  JOIN attribute s ON a0.userid = s.userid
+WHERE s.key = 'givenName' AND a0.userid IN (
+  SELECT a1.userid
+  FROM attribute a1
+  WHERE a1.key = 'l' AND a1.value ILIKE '%Berlin%'
+        AND exists(SELECT 1
+                   FROM attribute a2
+                   WHERE a2.userid = a1.userid AND a2.key = 'sn' AND a2.value ILIKE '%Muster%'
+                         AND exists(SELECT 1
+                                    FROM attribute a3
+                                    WHERE a3.userid = a1.userid AND a3.key = 'givenName' AND a3.value ILIKE '%a%'))
+  OFFSET 0
+  LIMIT 2)
+ORDER BY s.value DESC;

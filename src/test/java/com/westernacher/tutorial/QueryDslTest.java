522acbf3601115cf6b9b64bd68a9798eb1843e9f
Torsten Werner <torsten.werner@westernacher.com>
2017-04-26 22:37:08 +0200

    add a querydsl tuple test

--- src/test/java/com/westernacher/tutorial/QueryDslTest.java
+++ src/test/java/com/westernacher/tutorial/QueryDslTest.java
@@ -1,5 +1,6 @@
 package com.westernacher.tutorial;
 
+import com.querydsl.core.Tuple;
 import com.querydsl.sql.SQLQueryFactory;
 import com.westernacher.tutorial.querydsl.QAttribute;
 import org.junit.Test;
@@ -7,6 +8,7 @@ import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
@@ -18,9 +20,17 @@ public class QueryDslTest {
     private SQLQueryFactory queryFactory;
 
     @Test
-    public void attribute() throws Exception {
+    public void userId() throws Exception {
         final QAttribute attribute = new QAttribute("a");
         final List<String> userIds = queryFactory.select(attribute.userid).from(attribute).fetch();
+        System.out.println(userIds);
+    }
+
+    @Test
+    public void tuple() throws Exception {
+        final QAttribute attribute = new QAttribute("a");
+        final List<Tuple> userIds = queryFactory.select(attribute.userid, attribute.key, attribute.value)
+                .from(attribute).fetch();
         System.out.println(userIds);
     }
 }

18fef354d42e0a9b02692e72dee3c621b6c9b8bc
Torsten Werner <torsten.werner@westernacher.com>
2017-05-29 16:12:26 +0200

    finish MaybeSqlTest

--- maybesql.sql
+++ maybesql.sql
@@ -2,14 +2,16 @@
 
 SELECT a1.*
 FROM attribute a1
-WHERE a1.key = 'l' AND a1.value ILIKE '%Berlin%';
+WHERE a1.key = 'l' AND a1.value ILIKE '%Berlin%'
+ORDER BY a1.userid ASC;
 
 SELECT a0.*
 FROM attribute a0
 WHERE a0.userid IN (
   SELECT a1.userid
   FROM attribute a1
-  WHERE a1.key = 'l' AND a1.value ILIKE '%Berlin%');
+  WHERE a1.key = 'l' AND a1.value ILIKE '%Berlin%')
+ORDER BY a0.userid ASC;
 
 SELECT a0.*
 FROM attribute a0
@@ -19,7 +21,8 @@ WHERE a0.userid IN (
   WHERE a1.key = 'l' AND a1.value ILIKE '%Berlin%'
         AND exists(SELECT 1
                    FROM attribute a2
-                   WHERE a2.userid = a1.userid AND a2.key = 'sn' AND a2.value ILIKE '%Muster%'));
+                   WHERE a2.userid = a1.userid AND a2.key = 'sn' AND a2.value ILIKE '%Muster%'))
+ORDER BY a0.userid ASC;
 
 SELECT a0.*
 FROM attribute a0
@@ -32,7 +35,8 @@ WHERE a0.userid IN (
                    WHERE a2.userid = a1.userid AND a2.key = 'sn' AND a2.value ILIKE '%Muster%'
                          AND exists(SELECT 1
                                     FROM attribute a3
-                                    WHERE a3.userid = a1.userid AND a3.key = 'givenName' AND a3.value ILIKE '%Hans%')));
+                                    WHERE a3.userid = a1.userid AND a3.key = 'givenName' AND a3.value ILIKE '%Hans%')))
+ORDER BY a0.userid ASC;
 
 -- an ldap query to the sql query above:
 -- (&(l=*Berlin*)(sn=*Muster*)(givenName=*Hans*))
--- src/test/java/com/westernacher/tutorial/MaybeSqlTest.java
+++ src/test/java/com/westernacher/tutorial/MaybeSqlTest.java
@@ -1,5 +1,7 @@
 package com.westernacher.tutorial;
 
+import com.querydsl.core.types.dsl.BooleanExpression;
+import com.querydsl.core.types.dsl.Expressions;
 import com.querydsl.sql.SQLExpressions;
 import com.querydsl.sql.SQLQuery;
 import com.querydsl.sql.SQLQueryFactory;
@@ -12,6 +14,11 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+import static java.lang.String.format;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
@@ -27,24 +34,132 @@ public class MaybeSqlTest {
         final SQLQuery<Attribute> query = queryFactory.selectFrom(a1)
                 .from(a1)
                 .where(a1.key.eq("l")
-                        .and(a1.value.likeIgnoreCase("%Berlin%")));
+                        .and(a1.value.likeIgnoreCase("%Berlin%")))
+                .orderBy(a1.userid.asc());
         logger.info("query: {}", query.getSQL().getSQL());
-        logger.info("attributes: {}", query.fetch());
+        logAttributes(query.fetch());
     }
 
     @Test
     public void query02() throws Exception {
-        final QAttribute a0 = new QAttribute("a0");
         final QAttribute a1 = new QAttribute("a1");
+        final SQLQuery<String> subQuery = SQLExpressions.select(a1.userid)
+                .from(a1)
+                .where(a1.key.eq("l")
+                        .and(a1.value.likeIgnoreCase("%Berlin%")));
+        final QAttribute a0 = new QAttribute("a0");
         final SQLQuery<Attribute> query = queryFactory.selectFrom(a0)
                 .from(a0)
-                .where(a0.userid.in(
-                        SQLExpressions.select(a1.userid)
-                                .from(a1)
-                                .where(a1.key.eq("l")
-                                        .and(a1.value.likeIgnoreCase("%Berlin%")))
-                ));
+                .where(a0.userid.in(subQuery))
+                .orderBy(a0.userid.asc());
         logger.info("query: {}", query.getSQL().getSQL());
-        logger.info("attributes: {}", query.fetch());
+        logAttributes(query.fetch());
+    }
+
+    @Test
+    public void query03() throws Exception {
+        final QAttribute a1 = new QAttribute("a1");
+        final QAttribute a2 = new QAttribute("a2");
+        final SQLQuery<Integer> subSubQuery = SQLExpressions.select(Expressions.ONE)
+                .from(a2)
+                .where(a2.key.eq("sn")
+                        .and(a2.value.likeIgnoreCase("%Muster%"))
+                        .and(a2.userid.eq(a1.userid)));
+        final SQLQuery<String> subQuery = SQLExpressions.select(a1.userid)
+                .from(a1)
+                .where(a1.key.eq("l")
+                        .and(a1.value.likeIgnoreCase("%Berlin%"))
+                        .and(subSubQuery.exists()));
+        final QAttribute a0 = new QAttribute("a0");
+        final SQLQuery<Attribute> query = queryFactory.selectFrom(a0)
+                .from(a0)
+                .where(a0.userid.in(subQuery))
+                .orderBy(a0.userid.asc());
+        logger.info("query: {}", query.getSQL().getSQL());
+        logAttributes(query.fetch());
+    }
+
+    @Test
+    public void query04() throws Exception {
+        final QAttribute a1 = new QAttribute("a1");
+        final QAttribute a3 = new QAttribute("a3");
+        final SQLQuery<Integer> subSubSubQuery = SQLExpressions.select(Expressions.ONE)
+                .from(a3)
+                .where(a3.key.eq("givenName")
+                        .and(a3.value.likeIgnoreCase("%Hans%"))
+                        .and(a3.userid.eq(a1.userid)));
+        final QAttribute a2 = new QAttribute("a2");
+        final SQLQuery<Integer> subSubQuery = SQLExpressions.select(Expressions.ONE)
+                .from(a2)
+                .where(a2.key.eq("sn")
+                        .and(a2.value.likeIgnoreCase("%Muster%"))
+                        .and(a2.userid.eq(a1.userid)
+                                .and(subSubSubQuery.exists())));
+        final SQLQuery<String> subQuery = SQLExpressions.select(a1.userid)
+                .from(a1)
+                .where(a1.key.eq("l")
+                        .and(a1.value.likeIgnoreCase("%Berlin%"))
+                        .and(subSubQuery.exists()));
+        final QAttribute a0 = new QAttribute("a0");
+        final SQLQuery<Attribute> query = queryFactory.selectFrom(a0)
+                .from(a0)
+                .where(a0.userid.in(subQuery))
+                .orderBy(a0.userid.asc());
+        logger.info("query: {}", query.getSQL().getSQL());
+        logAttributes(query.fetch());
+    }
+
+    @Test
+    public void query05() throws Exception {
+        final QAttribute a0 = new QAttribute("a0");
+        final QAttribute a1 = new QAttribute("a1");
+        final QAttribute a2 = new QAttribute("a2");
+        final QAttribute a3 = new QAttribute("a3");
+        final QAttribute s0 = new QAttribute("s0");
+        final QAttribute s1 = new QAttribute("s1");
+
+        final BooleanExpression filterA3 = a3.userid.eq(a1.userid)
+                .and(a3.key.eq("givenName"))
+                .and(a3.value.likeIgnoreCase("%a%"));
+        final SQLQuery<Integer> subQueryA3 = SQLExpressions.select(Expressions.ONE)
+                .from(a3)
+                .where(filterA3);
+
+        final BooleanExpression filterA2 = a2.userid.eq(a1.userid)
+                .and(a2.key.eq("sn"))
+                .and(a2.value.likeIgnoreCase("%Muster%"))
+                .and(subQueryA3.exists());
+        final SQLQuery<Integer> subQueryA2 = SQLExpressions.select(Expressions.ONE)
+                .from(a2)
+                .where(filterA2);
+
+        final BooleanExpression filterA1 = s1.key.eq("givenName")
+                .and(a1.key.eq("l"))
+                .and(a1.value.likeIgnoreCase("%Berlin%"))
+                .and(subQueryA2.exists());
+        final SQLQuery<String> subQueryA1 = SQLExpressions.select(a1.userid)
+                .from(a1)
+                .join(s1).on(a1.userid.eq(s1.userid))
+                .where(filterA1)
+                .orderBy(s1.value.desc())
+                .offset(0)
+                .limit(2);
+
+        final BooleanExpression filterA0 = s0.key.eq("givenName").
+                and(a0.userid.in(subQueryA1));
+        final SQLQuery<Attribute> query = queryFactory.selectFrom(a0)
+                .join(s0).on(a0.userid.eq(s0.userid))
+                .where(filterA0)
+                .orderBy(s0.value.desc());
+
+        logger.info("query: {}", query.getSQL().getSQL());
+        logAttributes(query.fetch());
+    }
+
+    private void logAttributes(List<Attribute> attributes) {
+        final String message = attributes.stream()
+                .map(attribute -> format("[%s - %s: %s]", attribute.getUserid(), attribute.getKey(), attribute.getValue()))
+                .collect(Collectors.joining("\n"));
+        logger.info("{} attributes:\n{}", attributes.size(), message);
     }
 }
